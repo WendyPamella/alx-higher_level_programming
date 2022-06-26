2ce3edeb69a308ccf11013a997ee6052bdca7a11
root <wendypamella99@gmail.com>
2022-06-26 13:04:31 -0700

    square it

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""1-square.py"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """
+        Creates an instance odf a Square
+        Args:
+            size: the size of the square
+        """
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,4 @@
+python Classes
+
+In this directory we are going to look at python scripts to help us learn more about python classes and understand how python calsses operate
+
